5bbd64df224dbb472245a8364e16bace7fee7989
Mark Janssen -- Sig-I/O <mark@sig-io.nl>
2013-12-21 17:25:44 +0100

    Update for new html output format from ssllabs, use rating

--- check_ssllabs.sh
+++ check_ssllabs.sh
@@ -11,20 +11,18 @@ NSCACONF=/etc/send_nsca.cfg
 NSCASERVER=your.nagios.system
 HOSTNAME=`hostname`
 
-PERCENTAGE=`curl -k -s https://www.ssllabs.com/ssltest/analyze.html?d=${SITE} | grep -A 1 percentage_g | tail -1 | tr -d ' \n\r'`
+RATING=`curl -k -s https://www.ssllabs.com/ssltest/analyze.html?d=${SITE} | grep rating_g | tr '<>/' '   ' | awk '{print $3}' `
 
 OK=3
-if [[ ${PERCENTAGE} -ge 85 ]];
+
+if [[ "x${RATING}" == "xA" ]];
 then
         OK=0
+elif [[ "x${RATING}" == "xB" ]];
+then
+        OK=1;
 else
-        if [[ ${PERCENTAGE} -ge 75 ]];
-        then
-                OK=1;
-        else
-                OK=2;
-        fi
+        OK=2;
 fi
 
 echo "${HOSTNAME}	ssllabs_${SITE}	${OK}	SSLLabs score for ${SITE} = ${PERCENTAGE}" | ${NSCA} -c ${NSCACONF} -H ${NSCASERVER}
-
