a93a51870dfbcb85fd82a8efd31916f73c709d1c
Mark Janssen <mark@sig-io.nl>
2014-05-28 17:05:31 +0200

    Rework for ssllabs rating A(+|-)|B|C-F

--- check_ssllabs.sh
+++ check_ssllabs.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 # Query the ssllabs tests for a site
-# (C) 2012 Mark Janssen, Sig-I/O Automatisering
+# (C) 2012-2014 Mark Janssen, Sig-I/O Automatisering
 # License: CC-BY-3.0 http://creativecommons.org/licenses/by/3.0/
 
 SITE=$1
@@ -13,28 +13,25 @@ NSCASERVER=your.nagios.system
 HOSTNAME=`hostname`
 
 if [ -z ${SERVER} ]; then
-  SCORE=`curl -k -s "https://www.ssllabs.com/ssltest/analyze.html?d=${SITE}" | grep -A 1 "chartValue" | tr -d '\t' | grep -oE "[0-9]([0-9][0-9]|[0-9])"`
+  URL="https://www.ssllabs.com/ssltest/analyze.html?d=${SITE}"
 else
-  SCORE=`curl -k -s "https://www.ssllabs.com/ssltest/analyze.html?d=${SITE}&s=${SERVER}" | grep -A 1 "chartValue" | tr -d '\t' | grep -oE "[0-9]([0-9][0-9]|[0-9])"`
+  URL="https://www.ssllabs.com/ssltest/analyze.html?d=${SITE}&s=${SERVER}"
 fi
 
-TOTAL_SCORE=`echo ${SCORE} | tr -d '\n'`
+SCORE=`curl -k -s "${URL}" | grep rating_g | tr '<>/' '   ' | awk '{print $6}'`
+
 OK=3
 
-for PERCENTAGE in ${SCORE};
-do
-  if [ ${PERCENTAGE} -ge 85 ];
-  then
-    OK=0
-  else
-    if [ ${PERCENTAGE} -ge 75 ];
-    then
-      OK=1;
-    else
-      OK=2;
-  fi
-  break
+if [[ "x${SCORE}" == "xA" ]]; then
+  OK=0
+elif  [[ "x${SCORE}" == "xA+" ]]; then
+  OK=0
+elif  [[ "x${SCORE}" == "xA-" ]]; then
+  OK=0
+elif  [[ "x${SCORE}" == "xB" ]]; then
+  OK=1
+else
+  OK=2
 fi
-done
 
-echo "${HOSTNAME}	ssllabs_${SITE}	${OK}	SSLLabs score for ${SITE} = ${TOTAL_SCORE}" | ${NSCA} -c ${NSCACONF} -H ${NSCASERVER}
+echo "${HOSTNAME}	ssllabs_${SITE}	${OK}	SSLLabs rating for ${SITE} = ${SCORE}" | ${NSCA} -c ${NSCACONF} -H ${NSCASERVER}
